ee2c98f5fbd359aebf095fbfd6c0afded225df61
yaminocellist <58488672+yaminocellist@users.noreply.github.com>
2022-09-07 20:39:55 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -10,8 +10,10 @@
 #include <algorithm>
 
 #include "settings.h"
-#include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/special_functions/bessel.hpp>
-#include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/quadrature/gauss_kronrod.hpp>
+// #include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/special_functions/bessel.hpp>
+// #include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/quadrature/gauss_kronrod.hpp>
+#include <boost/math/special_functions/bessel.hpp>
+#include <boost/math/quadrature/gauss_kronrod.hpp>
 
 using std::vector;
 using vector2d = vector<vector <double>>;
@@ -19,7 +21,7 @@ using vector2d = vector<vector <double>>;
 void printvd (vector<double> vd);
 void printvd (vector<int> vd);
 void print2dv (vector2d v);
-int sum_mg (vector<int> mg, int it);
+int sum_mg (vector<int> mg, int g);
 int find_g_1 (int i, vector<int> mg, int f);
 int first_non_zero (vector<double> v);
 vector<int> rest_mg (vector<int> mg, int g);
