03c95013341772ba161a0e2e5c5b37ecf2b48b9d
yaminocellist <58488672+yaminocellist@users.noreply.github.com>
2022-09-07 20:39:05 +0800

    Update settings.cpp

--- settings.cpp
+++ settings.cpp
@@ -88,65 +88,38 @@ void set_param (Parameters &p, int argc, char *argv[]) {
  *  index 'i' and 'j' indicates one of the STATEs,
  *      'i' indicates state of current segment,
  *      'j' indicates state of its next segment,
- *  where i or j = 0 indicates no binding of any DNAs:
 ***********************************************************/
 /*****************************************************************************************
  *  Look-up table for non-zero Q_n values: 
  *  value 0 means impossible or not considered;
  *  value 1 means energy is zero;
- * 
- *  1. two bound states both of which belong to the same type of protein:
- *      i = 1,                 j = i + 1 ∈ (1, m1]:             K_n_1 * C_0_1;
- *      i ∈ (1, m1),           j = i + 1 ∈ (1, m1]:             K_n_1;
- *      i = m1 + 1,            j = i + 1 ∈ (m1 + 1, m2]:        K_n_2 * C_0_2;
- *      i ∈ (m1 + 1, m1 + m2), j = i + 1 ∈ (m1 + 1, m1 + m2]:   K_n_2;
- *      ...
- *  2. one bound state followed by a right unbound end:
- *      i = 1,                 j = ∑mg + 2:                     K_n_1 * C_0_1 * Unwrap;
- *      i ∈ (1, m1),           j = ∑mg + 2:                     K_n_1 * Unwrap;
- *      i = m1 + 1,            j = ∑mg + 2:                     K_n_2 * C_0_2 * Unwrap;
- *      i ∈ (m1 + 1, m1 + m2), j = ∑mg + 2:                     K_n_1 * Unwrap;
- *      ...
- *  3. one bound state of type-g1 followed by another bound state of type-g2:
- *      i = 1,                 j = m1 + 1:                      K_n_1 * w_g1_g2 * Unwrap * C_0_1 * C_0_2;
- *      i = 1,                 j ∈ (m1 + 1, m2]:                K_n_1 * w_g1_g2 * Unwrap * C_0_1;
- *  
- *  n. one unbound state followed by a left unbound end:
- *      i = 0,                 j = ∑mg + 1:                        1;
 ******************************************************************************************/
-// void set_Q (Parameters &p) {
-//     for (int n = 0; n < p.Q_n.size(); n++) {
-//         for (int i = 0; i < p.Q_n[n].size(); i++) {
-//             for (int j = 0; j < p.Q_n[n][i].size(); j++) {
-//             /*  General assignments:  */
-//             if (i == 0) {
-//                 p.Q_n[n][i][i] = 1.;
-//             }
-//             if (i == 1 + + sum_mg(p.mg, g - 1)) {
-//                 if (j == sum_mg(p.mg, p.f) + 2) {
-//                     p.Q_n[n][i][j] = p.K_n_g[n][g] * p.C_0_g[g] * p.Unwrap[g];
-//                 }
-//                 else if (j == sum_mg(p.mg, g)) {
-//                     p.Q_n[n][i][j] = p.K_n_g[n][g] * p.w_g1_g2[g][g + 1] * p.Unwrap[g] * p.C_0_g[g] * p.C_0_g[g];
-//                 }
-//                 else {
-//                     p.Q_n[n][i][i + 1] = p.K_n_g[n][g] * p.C_0_g[g];
-//                 }
-//             }
-//             /*  For all-or-none binding unit of type mg proteins:  */
-//             // if (i >= 1 + sum_mg(p.mg, g - 1) && i <= sum_mg(p.mg, g)) {
-//             // if (i == 1 + sum_mg(p.mg, g - 1)) {
-//             //     p.Q_n[n][i][i + 1] = p.K_n_g[n][g] * p.C_0_g[g]; 
-//             // }
-//             // else {
-//             //     p.Q_n[n][i][i + 1] = p.K_n_g[n][g];
-//             // }
-//             }         
-            
-//         }
-//     }
-// }
+void set_Q (Parameters &p) {
+    for (int n = 0; n < p.Q_n.size(); n++) {
+        for (int i = 0; i < p.Q_n[n].size(); i++) {
+            for (int j = 0; j < p.Q_n[n][i].size(); j++) {
+                /*  Table 1.:  */
+                for (int g = 0; g < p.f; g++) {
+                    if (i == sum_mg(p.mg, g)) {
+                        if (n == 0) {
+                            p.Q_n[n][i][i + 1] = p.K_n_g[n][g] * p.C_0_g[g];
+                        }
+                        else {
+                            p.Q_n[n][i][i + 1] = p.K_n_g[n][g];
+                        }
+                    }
+                }
 
+                /*  Table 2.:  */
+                for (int g = 0; g < p.f; g++) {
+                    if (i > sum_mg(p.mg, g) && i < sum_mg(p.mg, g + 1) - 1) {
+                        p.Q_n[n][i][i + 1] = p.K_n_g[n][g];
+                    }
+                }
+            }
+        }
+    }
+}
 /*******************************************************************
  *   1st argument: (mg) binding site length of 1st type of protein;
  *   2nd argument: (Vg) max interaction distance of 1st type of protein;
@@ -158,13 +131,12 @@ void set_param (Parameters &p, int argc, char *argv[]) {
 int main(int argc, char *argv[]) {
     Parameters p;
     set_param(p, argc, argv);
-    
-    // print2dv(p.K_n_g);
+    std::cout << "The results:" << std::endl;
 
-    vector<int> mmg = rest_mg(p.mg, 4);
-    printvd(mmg);
-    printvd(p.mg);
-    // std::cout << sum_mg(p.mg, -1) << ' ' << sum_mg(p.mg, 0) << std::endl;
+    std::cout << p.f << std::endl;
+    std::cout << sum_mg(p.mg, 0) << ' ' << sum_mg(p.mg, 1) << std::endl;
+    std::cout << sum_mg(p.mg, 1) << ' ' << sum_mg(p.mg, 2) << std::endl;
+    std::cout << sum_mg(p.mg, 2) << ' ' << sum_mg(p.mg, 3) << std::endl;
     // std::cout << p.Q_n[144][99][95] << std::endl;
     // std::cout << first_non_zero(p.K_n_g[7]) << std::endl;
 }
