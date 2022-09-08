bea9684f064990287b52d7c61234902a9eff954b
yaminocellist <58488672+yaminocellist@users.noreply.github.com>
2022-09-09 03:04:34 +0800

    Update settings.cpp

--- settings.cpp
+++ settings.cpp
@@ -63,6 +63,12 @@ void set_param (Parameters &p, int argc, char *argv[]) {
         p.w_g1_g2[i].resize(p.f, 1);
     }
 
+    /*  Upper limit of bound states:  */
+    p.bound_cap = p.sum_mg(p.f); 
+
+    /*  Upper limit of g1-g2 gap states:  */
+    p.gap_cap = p.sum_mgVg(p.f);
+
     // Calculate the size of Q_n:
     for (int i = 0; i < argc - 1; i++) {
         if (((i + 1) % 4) == 1 || ((i + 1) % 4) == 2)
@@ -75,12 +81,12 @@ void set_param (Parameters &p, int argc, char *argv[]) {
     for (int i = 0; i < p.N; i++) {
         p.Q_n[i].resize(p.dimensions);
         for (int j = 0; j < p.dimensions; j++) {
-            p.Q_n[i][j].resize(p.dimensions, 0.);
+            p.Q_n[i][j].resize(p.dimensions, 999.);
         }
     }
     
     /*  Set value for each elements of Q_n:  */
-    // set_Q(p);
+    set_Q(p);
 }
 
 /***********************************************************
@@ -100,7 +106,7 @@ void set_Q (Parameters &p) {
             for (int j = 0; j < p.Q_n[n][i].size(); j++) {
                 /*  Table 1.:  */
                 for (int g = 0; g < p.f; g++) {
-                    if (i == sum_mg(p.mg, g)) {
+                    if (i == p.sum_mg(g)) {
                         if (n == 0) {
                             p.Q_n[n][i][i + 1] = p.K_n_g[n][g] * p.C_0_g[g];
                         }
@@ -112,31 +118,117 @@ void set_Q (Parameters &p) {
 
                 /*  Table 2.:  */
                 for (int g = 0; g < p.f; g++) {
-                    if (i > sum_mg(p.mg, g) && i < sum_mg(p.mg, g + 1) - 1) {
+                    if (i > p.sum_mg(g) && i < p.sum_mg(g + 1) - 1) {
                         p.Q_n[n][i][i + 1] = p.K_n_g[n][g];
+                    }
+                }
+
+                /*  Table 3.:  */
+                if (i < p.bound_cap) {
+                    int temp_g = p.find_g_1(i);
+                    if (n == 0) {
+                        p.Q_n[n][i][p.bound_cap + 1] = p.K_n_g[n][temp_g] * p.C_0_g[temp_g] * p.Unwrap[temp_g];
+                    }
+                    else {
+                        p.Q_n[n][i][p.bound_cap + 1] = p.K_n_g[n][temp_g] * p.Unwrap[temp_g];
+                    }
+                }
+
+                /*  Table 4.:  */
+                if (i < p.bound_cap) {
+                    int temp_g1 = p.find_g_1(i);
+                    int temp_g2 = p.find_g_1(j);
+                    if (j == p.sum_mg(temp_g2)) {
+                        if (n == 0) {
+                            p.Q_n[n][i][j] = p.K_n_g[n][temp_g1]*p.w_g1_g2[temp_g1][temp_g2]*p.Unwrap[temp_g1]*p.C_0_g[temp_g1]*p.C_0_g[temp_g2];
+                        }
+                        else {
+                            p.Q_n[n][i][j] = p.K_n_g[n][temp_g1]*p.w_g1_g2[temp_g1][temp_g2]*p.Unwrap[temp_g1]*p.C_0_g[temp_g1];
+                        }
+                    }
+                }
+
+                /*  Table 5.:  */
+                if (i < p.bound_cap && j < p.bound_cap) {
+                    int temp_g1 = p.find_g_1(i);
+                    int temp_g2 = p.find_g_1(j);
+                    if (i == p.sum_mg(temp_g1 + 1) - 1 && (j > p.sum_mg(temp_g2) && j < p.sum_mg(temp_g2 + 1))) {
+                        p.Q_n[n][i][j] = p.K_n_g[n][temp_g1]*p.w_g1_g2[temp_g1][temp_g2]*p.Unwrap[temp_g2]*p.C_0_g[temp_g2];
+                    }
+                }
+
+                /*  Table 6.:  */
+                if (i == p.bound_cap && j == p.bound_cap) {
+                    p.Q_n[n][i][j] = 1.;
+                }
+
+                /*  Table 7.:  */
+                if (i == p.bound_cap + 1 && j == p.bound_cap + 1) {
+                    p.Q_n[n][i][j] = 1.;
+                }
+
+                /*  Table 8.:  */
+                if (i == p.bound_cap && j < p.bound_cap) {
+                    int g2 = p.find_g_1(j);
+                    p.Q_n[n][i][j] = p.w_g1_g2[0][g2]*p.Unwrap[g2]*p.C_0_g[g2];
+                }
+
+                /*  Table 9.:  */
+                if (i < p.bound_cap) {
+                    int g1 = p.find_g_1(i);
+                    if (j == p.gap_cap + 1) {
+                        if (n == 1) {
+                            p.Q_n[n][i][j] = p.w_g1_g2[g1][0]*p.K_n_g[0][g1]*p.Unwrap[g1]*p.C_0_g[g1];
+                        }
+                        else {
+                            p.Q_n[n][i][j] = p.w_g1_g2[g1][0]*p.K_n_g[0][g1];
+                        }
+                    }
+                }
+
+                /*  Table 10.:  */
+                if (i > p.gap_cap && i <= p.dimensions - 2) {
+                    p.Q_n[n][i][i + 1] = 1.;
+                }
+
+                /*  Table 11.:  */
+                if (i == p.dimensions - 1) {
+                    p.Q_n[n][i][i] = 1.;
+                }
+
+                /*  Table 12.:  */
+                if (i == p.dimensions - 1 && j < p.bound_cap) {
+                    int g2 = p.find_g_1(j);
+                    p.Q_n[n][i][j] = p.Unwrap[g2]*p.C_0_g[g2];
+                }
+
+                /*  Table 13.:  */
+                if (i < p.bound_cap && (j > p.bound_cap + 1 && j <= p.gap_cap)) {
+                    int g1 = p.find_g_1(i);
+                    int g2 = p.find_g_2(j);
+                    if (n == 0) {
+                        p.Q_n[n][i][j] = p.w_g1_g2[g1][g2]*p.K_n_g[n][g1]*p.Unwrap[g1]*p.C_0_g[g1];
+                    }
+                    else {
+                        p.Q_n[n][i][j] = p.w_g1_g2[g1][g2]*p.K_n_g[n][g1]*p.Unwrap[g1];
+                    }
+                }
+
+                /*  Table 14.:  */
+                if (i > p.bound_cap + 2 && i <= p.gap_cap) {
+                    p.Q_n[n][i][i - 1] = 1;
+                }
+
+                /*  Table 15.:  */
+                if ((i > p.bound_cap + 1 && i <= p.gap_cap) && j < p.bound_cap) {
+                    int g1 = p.find_g_2(i);
+                    int g2 = p.find_g_1(j);
+                    if (i == p.sum_mgVg(g1) + 1) {
+                        p.Q_n[n][i][j] = p.Unwrap[g2]*p.C_0_g[g2];
                     }
                 }
             }
         }
     }
 }
-/*******************************************************************
- *   1st argument: (mg) binding site length of 1st type of protein;
- *   2nd argument: (Vg) max interaction distance of 1st type of protein;
- *   3rd argument: (K_n_g) binding constant for 1st type of protein;
- *   4th argument: (C_0_g) concentration of 1st type of protein;
- *   ... ...
- *   and so on:
-********************************************************************/
-int main(int argc, char *argv[]) {
-    Parameters p;
-    set_param(p, argc, argv);
-    std::cout << "The results:" << std::endl;
 
-    std::cout << p.f << std::endl;
-    std::cout << sum_mg(p.mg, 0) << ' ' << sum_mg(p.mg, 1) << std::endl;
-    std::cout << sum_mg(p.mg, 1) << ' ' << sum_mg(p.mg, 2) << std::endl;
-    std::cout << sum_mg(p.mg, 2) << ' ' << sum_mg(p.mg, 3) << std::endl;
-    // std::cout << p.Q_n[144][99][95] << std::endl;
-    // std::cout << first_non_zero(p.K_n_g[7]) << std::endl;
-}
