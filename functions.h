d0d7c35b073b4926d9a4600d0cc655f7946d3006
yaminocellist <58488672+yaminocellist@users.noreply.github.com>
2022-09-09 03:02:56 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -10,19 +10,22 @@
 #include <algorithm>
 
 #include "settings.h"
-// #include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/special_functions/bessel.hpp>
-// #include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/quadrature/gauss_kronrod.hpp>
-#include <boost/math/special_functions/bessel.hpp>
-#include <boost/math/quadrature/gauss_kronrod.hpp>
+#include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/special_functions/bessel.hpp>
+#include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/math/quadrature/gauss_kronrod.hpp>
+// #include <boost/math/special_functions/bessel.hpp>
+// #include <boost/math/quadrature/gauss_kronrod.hpp>
 
 using std::vector;
 using vector2d = vector<vector <double>>;
 
+
+
 void printvd (vector<double> vd);
 void printvd (vector<int> vd);
 void print2dv (vector2d v);
-int sum_mg (vector<int> mg, int g);
-int find_g_1 (int i, vector<int> mg, int f);
+void print3dv (vector3d v);
+// int sum_mg (int g);
+// int find_g_1 (int i, vector<int> mg, int f);
 int first_non_zero (vector<double> v);
 vector<int> rest_mg (vector<int> mg, int g);
 #endif
