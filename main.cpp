8a1e93ac8eed17b49a188fed7508c5ec569d36c9
yaminocellist <58488672+yaminocellist@users.noreply.github.com>
2022-09-09 19:45:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "settings.h"
 #include "functions.h"
+#include "main.h"
+#include <eigen3/Eigen/StdVector>
 
 /*******************************************************************
  *   1st argument: (mg) binding site length of 1st type of protein;
@@ -9,12 +11,29 @@
  *   ... ...
  *   and so on:
 ********************************************************************/
-
-
-
 int main(int argc, char *argv[]) {
+    Timer global;
     Parameters p;
     set_param(p, argc, argv);
+
+    std::cout << "The results:" << std::endl;
+    
+    MatrixXd M = p.Q_n[0];
+    for (int i = 1; i < p.Q_n.size(); i++) {
+        M = M*p.Q_n[i];
+    }
+    
+    std::cout << "The Qn matrix is: " << std::endl << M << std::endl;
+    /***********************************************************
+    *   Summing all elements of Qn to get partition function Z:
+    ***********************************************************/
+    double Z = 0.;
+    for (int i = 0; i < M.rows(); i++) {
+        for (int j = 0; j < M.cols(); j++) {
+            Z += M(i, j); 
+        }
+    }
+    std::cout << "The partition function result is: " << Z << std::endl;
 
     /********************************************
     *   Claiming input variables:
@@ -23,9 +42,7 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i< p.f; i++) {
         std::cout << "Type " << i+1 << ", g = " << i << ", mg[" << i+1 << "] = " << p.mg[i] << ", Vg[" << i+1 << "] = " << p.Vg[i] << ";" << std::endl;
     }
+    double runtime = global.elapsed();
     
-    std::cout << "The results:" << std::endl;
-    // std::cout << p.sum_mgVg(0) << "  " << p.sum_mgVg(1) << "  " << p.sum_mgVg(2) << "  " << p.sum_mgVg(3) << std::endl;
-    // std::cout << "It should be:" << std::endl << "20  26  39  40" << std::endl; 
-    std::cout << p.sum_mgVg(2)+1 << std::endl;
+    std::cout << "Number of DNA segments is " << p.N << ", total elapsed time is " << runtime << " seconds.\n";
 }
